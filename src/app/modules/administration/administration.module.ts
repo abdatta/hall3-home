a2f7dda1cc501034a1a8c4e4470ff967defa9ceb
Abhishek Datta <abdatta@iitk.ac.in>
2024-03-17 16:54:47 +0530

    Fix ngx-cropper duplicate

--- src/app/modules/administration/administration.module.ts
+++ src/app/modules/administration/administration.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AutoSizeInputModule } from 'ngx-autosize-input';
-import { NgxCropperModule } from '../../../../ngx-cropper/ngx-cropper.module';
+import { NgxCropperModule } from '../ngx-cropper/ngx-cropper.module';
 import { AdministrationRouterModule } from './administration.router';
 import { InfoheadModule } from '../infohead/infohead.module';
 import { LoaderModule } from '../loader/loader.module';
